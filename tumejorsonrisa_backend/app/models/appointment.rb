e339448f8d675799c00a5317a73298dcead055be
AlanWadith01 <127111674+AlanWadith01@users.noreply.github.com>
2024-09-10 11:58:03 -0500

    Delete tumejorsonrisa_backend directory

--- tumejorsonrisa_backend/app/models/appointment.rb
+++
@@ -1,7 +0,0 @@
-# app/models/appointment.rb
-class Appointment < ApplicationRecord
-    belongs_to :patient
-  
-    validates :tipoId, :id, :nombre, :odontologo, :fecha, :hora, :descripcion, presence: true
-  end
-  
--- tumejorsonrisa_backend/db/migrate/20240909071803_create_appointments.rb
+++
@@ -1,8 +0,0 @@
-class CreateAppointments < ActiveRecord::Migration[7.1]
-  def change
-    create_table :appointments do |t|
-
-      t.timestamps
-    end
-  end
-end
